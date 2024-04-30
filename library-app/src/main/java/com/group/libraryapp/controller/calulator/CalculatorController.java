f7168672c0f33256efd4e896e55f183cb25683df
cyPark95 <parkcy1007@gmail.com>
2024-04-30 09:16:50 +0900

    feat: 두 개의 숫자를 곱하는 POST API 개발

--- library-app/src/main/java/com/group/libraryapp/controller/calulator/CalculatorController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/calulator/CalculatorController.java
@@ -1,9 +1,8 @@
 package com.group.libraryapp.controller.calulator;
 
 import com.group.libraryapp.controller.dto.Calculator.request.CalculatorAddRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RestController;
+import com.group.libraryapp.controller.dto.Calculator.request.CalculatorMultiplyRequest;
+import org.springframework.web.bind.annotation.*;
 
 // 해당 Class를 Controller로 등록한다.
 // API 진입 지점으로 선언
@@ -20,5 +19,15 @@ public class CalculatorController {
             @ModelAttribute CalculatorAddRequest request
     ) {
         return request.number1() + request.number2();
+    }
+
+    // HTTP Method: POST
+    // HTTP Path: /multiply
+    @PostMapping("/multiply")
+    public int multiplyTowNumbers(
+            // HTTP Body에 있는 JSON 데이터를 파싱해서 객체로 바인딩
+            @RequestBody CalculatorMultiplyRequest request
+    ) {
+        return request.number1() * request.number2();
     }
 }
---
+++ library-app/src/main/java/com/group/libraryapp/controller/dto/Calculator/request/CalculatorMultiplyRequest.java
@@ -0,0 +1,7 @@
+package com.group.libraryapp.controller.dto.Calculator.request;
+
+public record CalculatorMultiplyRequest(
+        int number1,
+        int number2
+) {
+}
