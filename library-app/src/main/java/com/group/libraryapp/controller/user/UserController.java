81041eb1b1a3a2f1a6e16e4642737076317c9fdc
cyPark95 <parkcy1007@gmail.com>
2024-05-01 11:58:58 +0900

    feat: Spring 프로젝트에 Database 적용

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,32 +1,45 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.User;
 import com.group.libraryapp.dto.user.request.UserCreateReqeust;
 import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 // @RestController 어노테이션을 사용하면 Body에 데이터를 포함하여 응답을 전송할 수 있다.
 @RestController
 public class UserController {
 
-    private final List<User> users = new ArrayList<>();
+    private static final String TABLE = "user";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user")  // POST /user
     public void saveUser(@RequestBody UserCreateReqeust request) {
-        users.add(new User(request.name(), request.age()));
+        // 값이 들어가는 부분에 ?를 사용하면 동적으로 값을 넣어줄 수 있다.
+        String sql = String.format("INSERT INTO %s (name, age) VALUE (?, ?);", TABLE);
+        // JdbcTemplate의 update() 메서드를 사용하면 INSERT, UPDATE, DELETE 쿼리를 사용할 수 있다.
+        jdbcTemplate.update(sql, request.name(), request.age());
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        return IntStream.range(0, users.size())
-                .mapToObj(i -> UserResponse.from(i + 1, users.get(i)))
-                .toList();
+        String sql = String.format("SELECT * FROM %s", TABLE);
+        // JdbcTemplate의 query() 메서드를 사용하면 SELECT 쿼리를 사용할 수 있다.
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+
+            return new UserResponse(id, name, age);
+        });
     }
 }
---
+++ library-app/src/main/resources/ddl/script.sql
@@ -0,0 +1,21 @@
+# 데이터베이스 생성
+CREATE DATABASE library;
+# 데이터베이스 목록 조회
+SHOW DATABASES;
+
+# 데이터베이스 선택
+USE library;
+
+# 테이블 생성
+CREATE TABLE user
+(
+    # AUTO_INCREMENT 설정해준 필드는 데이터를 명시적으로 집어 넣지 않더라도 1부터 1씩 증가하며 자동 저장
+    id   BIGINT AUTO_INCREMENT,
+    name VARCHAR(20),
+    age  INT,
+
+    # id 필드를 유일한 키로 지정
+    PRIMARY KEY (id)
+);
+
+SHOW TABLES;
