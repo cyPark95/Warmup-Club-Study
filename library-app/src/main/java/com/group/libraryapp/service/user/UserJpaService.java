59cb01fb815ca09ccf9a62625fb7fed23a27b8b1
cyPark95 <parkcy1007@gmail.com>
2024-05-09 09:09:41 +0900

    feat: UserJpaService 트랜잭션 적용

--- library-app/src/main/java/com/group/libraryapp/service/user/UserJpaService.java
+++ library-app/src/main/java/com/group/libraryapp/service/user/UserJpaService.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateReqeust;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,27 +19,35 @@ public class UserJpaService {
         this.userRepository = userRepository;
     }
 
+    // 아래 있는 메서드가 시작될 때 START TRANSACTION;을 해준다.(트랙잭션 시작)
+    // 메서드가 예외 없이 끝났다면 COMMIT
+    // 혹시라도 예외가 발생한다면 ROLLBACK(IOException과 같은 Checked Exception은 롤백이 일어나지 않는다.)
+    @Transactional
     public void saveUser(UserCreateReqeust request) {
         User user = new User(request.name(), request.age());
         userRepository.save(user);
     }
 
+    // readOnly 속성을 사용하면 데이터 변경을 위한 불필요한 기능이 빠지기 때문에 성능적 이점이 있다.
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponse::from)
                 .toList();
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         // SELECT * FROM user WHERE id = ?;
         // Optional<User>
         User user = userRepository.findById(request.id())
                 .orElseThrow(IllegalArgumentException::new);
 
+        // 변경 감지
         user.updateName(request.name());
-        userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name) {
         // SELECT * FROM user WHERE name = ?;
         User user = userRepository.findByName(name)
