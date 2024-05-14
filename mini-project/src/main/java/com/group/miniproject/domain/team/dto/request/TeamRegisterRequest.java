42fbef2c34ee829e88100a3430028f2fe2551b00
cyPark95 <parkcy1007@gmail.com>
2024-05-14 20:48:00 +0900

    feat: 팀 등록 HTTP 통신 로직 구현 & 테스트
    - 요청 데이터에 대한 유효성 검사 & Validation 어노테이션 추가
    - 테스트를 위한 H2 DB 의존성 추가 & yml 설정 파일 생성
    - 팀 등록 성공과 요청 데이터 유효성 검사에 대한 테스트

--- mini-project/src/main/java/com/group/miniproject/domain/team/dto/request/TeamRegisterRequest.java
+++ mini-project/src/main/java/com/group/miniproject/domain/team/dto/request/TeamRegisterRequest.java
@@ -4,6 +4,7 @@ import com.group.miniproject.domain.team.entity.Team;
 import jakarta.validation.constraints.NotBlank;
 
 public record TeamRegisterRequest(
+        @NotBlank(message = "팀 이름은 null 또는 공백일 수 없습니다.")
         String name
 ) {
 
---
+++ mini-project/src/test/java/com/group/miniproject/presentation/TeamControllerTest.java
@@ -0,0 +1,60 @@
+package com.group.miniproject.presentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.group.miniproject.domain.team.dto.request.TeamRegisterRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("팀 Controller 검증")
+@SpringBootTest
+@AutoConfigureMockMvc
+class TeamControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DisplayName("팀 등록 성공")
+    @Test
+    void registerTeam() throws Exception {
+        // given
+        TeamRegisterRequest request = new TeamRegisterRequest("name");
+
+        // when
+        // then
+        mockMvc.perform(post("/api/v1/teams")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("팀 등록 실패 - 유효하지 않은 Parameter")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void registerTeam_invalidParameter(String name) throws Exception {
+        // given
+        TeamRegisterRequest request = new TeamRegisterRequest(name);
+
+        // when
+        // then
+        mockMvc.perform(post("/api/v1/teams")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+}
