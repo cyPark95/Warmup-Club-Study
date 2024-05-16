8ef6a247e30158a37c5d0cc6b20509d92312046e
cyPark95 <parkcy1007@gmail.com>
2024-05-17 08:58:17 +0900

    docs: employee 테이블 생서 DDL team 외래키 설정 추가

--- mini-project/sql/ddl.sql
+++ mini-project/sql/ddl.sql
@@ -11,6 +11,7 @@ CREATE TABLE `team`
 CREATE TABLE `employee`
 (
     `id`              BIGINT AUTO_INCREMENT COMMENT '식별 값(PK)',
+    `team_id`         BIGINT      NOT NULL COMMENT '팀 식별 값(FK)',
     `name`            VARCHAR(50) NOT NULL COMMENT '이름',
     `role`            VARCHAR(20) NOT NULL COMMENT '역할',
     `work_start_date` DATE        NOT NULL COMMENT '회사에 들어온 날짜',
@@ -18,5 +19,6 @@ CREATE TABLE `employee`
     `created_at`      DATETIME    NOT NULL COMMENT '생성 날짜',
     `updated_at`      DATETIME    NOT NULL COMMENT '최종 수정 날짜',
 
-    PRIMARY KEY (`id`)
-)
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`team_id`) REFERENCES team (id)
+);
